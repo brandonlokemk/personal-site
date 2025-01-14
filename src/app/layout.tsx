aca551c79e51094d138fbd67ac50fd24364c345d
Brandon Loke <brandonlokemk@gmail.com>
2025-01-14 16:32:42 +0800

    Changed font to raleway

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import Link from 'next/link'
 import './globals.css';
 import { ReactNode } from 'react';
 
+// Font
+import { Raleway } from 'next/font/google';
+const raleway = Raleway({ weight: ['300'], subsets: ['latin'] })
+
+
 type RootLayoutProps = {
   children: ReactNode;
 };
@@ -19,7 +24,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <Link href="/about" className="mx-2 hover:underline">About Me</Link>
           </nav>
         </header>
+        {/* <main className={open_sans.className}>{children}</main> */}
         <main>{children}</main>
+
         <footer className="p-4 bg-gray-800 text-white text-center">
           <p><a href="Brandon_Resume.pdf " download="Resume.pdf" className="mx-2 hover:underline">Download resume</a></p>
         </footer>
