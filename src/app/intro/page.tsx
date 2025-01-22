6e77ddf69bc0fb1ad91fbf5c46a5c46bec012e95
Brandon Loke <brandonlokemk@gmail.com>
2025-01-22 16:04:46 +0800

    .

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Intro() {
     return (
         <section id="intro" className="flex flex-col items-center justify-center min-h-screen">
-            <h2 className="text-4xl font-bold mb- text-center">Hi, I'm Brandon.<br/>Welcome to My Portfolio</h2>
+            <h2 className="text-4xl font-bold mb- text-center">Hi, I&apos;m Brandon.<br/>Welcome to My Portfolio</h2>
             <p className="text-lg mb-4 mt-3">Explore my projects and get to know my work.</p>
             <Link href="#about" className="px-6 py-3 bg-gray-500 text-white rounded-lg">Get to know me!</Link>
         </section>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 // File: app/layout.tsx (Root Layout)
-import Link from 'next/link'
 import './globals.css';
 import { ReactNode } from 'react';
-import { DownloadResumeButton } from '@/components/DownloadResumeButton';
-import { ThemeToggleButton } from '@/components/ThemeToggleButton';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 // Font
 import { Raleway } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
@@ -21,35 +17,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <div className={raleway.className}>
         <ThemeProvider attribute="class" defaultTheme='system' enableSystem disableTransitionOnChange>
           <body className="min-h-screen bg-primary-light dark:bg-primary-dark text-gray-800">
-              {/* <header className="flex flex-row items-center p-4 bg-secondary-light dark:bg-secondary-dark text-white">
-                <div className='flex items-center justify-between'>
-                  <div className='pr-4'>
-                    <Avatar>
-                      <AvatarImage src="htxtps://github.com/shadcn.png" />
-                      <AvatarFallback>BL</AvatarFallback>
-                    </Avatar>
-                  </div>
-                  <h1 className="text-md sm:text-2xl font-bold text-center">Brandon Loke</h1>
-                </div>
-                <div className='flex ml-auto'>
-                  <nav className="ml-auto flex items-center">
-                      <Link href="/" className="text-xs mx-2 hover:underline align-text-top">Home</Link>
-                      <Link href="/projects" className="text-xs mx-2 hover:underline align-text-top">Projects</Link>
-                      <Link href="/about" className="text-xs text-wrap ml-2 hover:underline align-text-top">About Me</Link>
-                  </nav>
-                  <div className='pl-4 justify-between space-x-2'>
-                    <DownloadResumeButton/>
-                    <ThemeToggleButton/>
-                  </div>
-                </div>                
-              </header> */}
-              {/* <main className={open_sans.className}>{children}</main> */}
-              <main className="scroll-smooth">{children}</main>
-
-              {/* <footer className="p-4 bg-gray-700 text-white text-center flex flex-row justify-center ">
-                <p><a href="Brandon_Resume.pdf " download="Resume.pdf" className="mx-2 hover:underline">Download resume</a></p>
-              </footer> */}
-            
+              <main>{children}</main>
           </body>
         </ThemeProvider>
       </div>
