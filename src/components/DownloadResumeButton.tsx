355dcbe20d6cd688c2228fd72e7ae0a2f19e0c60
Brandon Loke <brandonlokemk@gmail.com>
2025-06-26 12:15:17 +0800

    changed resume

--- src/components/DownloadResumeButton.tsx
+++ src/components/DownloadResumeButton.tsx
@@ -1,10 +1,26 @@
+'use Client';
+
 import { Button } from "./ui/button";
 import { FileDown } from "lucide-react"
 
 export function DownloadResumeButton ({ className }: { className?: string }) {
+    // const handleDownload = () => {
+    //     const link = document.createElement('a');
+    //     link.href = '/Brandon_Resume.pdf'; // Adjust the path to your resume file
+    //     link.download = 'Brandon_Resume.pdf'; // The name of the downloaded file
+    //     document.body.appendChild(link);
+    //     link.click();
+    //     document.body.removeChild(link);
+    // };
     return (
-        <Button className={className} size={"sm"}>
-            <FileDown/>
-        </Button>
+        <a className={className}
+        href="/Brandon_Resume.pdf"
+        download="Brandon_Resume.pdf"
+        >
+            <Button size={"sm"}>
+                <FileDown/>
+                Resume
+            </Button>
+        </a>
     )
 }
