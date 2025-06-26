7e7edabba64b2b184dc3ca0bfe110ef2f0fa3157
Brandon Loke <brandonlokemk@gmail.com>
2025-06-26 10:40:46 +0800

    removed project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,24 +1,24 @@
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
+// import {
+//     Dialog,
+//     DialogContent,
+//     DialogDescription,
+//     DialogHeader,
+//     DialogTitle,
+//     DialogTrigger,
+//   } from "@/components/ui/dialog"
 
-export default function ProjectCard() {
-    <Dialog>
-    <DialogTrigger>Open</DialogTrigger>
-    <DialogContent>
-        <DialogHeader>
-        <DialogTitle>Are you absolutely sure?</DialogTitle>
-        <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-        </DialogDescription>
-        </DialogHeader>
-    </DialogContent>
-    </Dialog>
-}
+// export default function ProjectCard() {
+//     <Dialog>
+//     <DialogTrigger>Open</DialogTrigger>
+//     <DialogContent>
+//         <DialogHeader>
+//         <DialogTitle>Are you absolutely sure?</DialogTitle>
+//         <DialogDescription>
+//             This action cannot be undone. This will permanently delete your account
+//             and remove your data from our servers.
+//         </DialogDescription>
+//         </DialogHeader>
+//     </DialogContent>
+//     </Dialog>
+// }
 
